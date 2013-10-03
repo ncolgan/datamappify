070a073dd6a5b646be638ec37c664471716e859b
Nick Colgan <nick.colgan@code42.com>
2013-10-03 13:24:31 -0500

    Rails 3 support.

--- lib/datamappify/entity.rb
+++ lib/datamappify/entity.rb
@@ -7,6 +7,31 @@ require 'datamappify/entity/composable'
 require 'datamappify/entity/association'
 require 'datamappify/entity/inspectable'
 
+module ActiveModel
+  module Model
+    def self.included(base) #:nodoc:
+      base.class_eval do
+        extend  ActiveModel::Naming
+        extend  ActiveModel::Translation
+        include ActiveModel::Validations
+        include ActiveModel::Conversion
+      end
+    end
+
+    def initialize(params={})
+      params.each do |attr, value|
+        self.public_send("#{attr}=", value)
+      end if params
+
+      super()
+    end
+
+    def persisted?
+      false
+    end
+  end
+end
+
 module Datamappify
   module Entity
     def self.included(klass)
